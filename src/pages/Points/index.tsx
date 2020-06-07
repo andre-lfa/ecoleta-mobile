3bc3df49a2609cfb2e57c16ba9eec9b0cdd2d6b6
Andre LFA <andrelfaareas@gmail.com>
2020-06-07 01:08:58 -0300

    Cria a função responsável por lidar com os items selecionados

--- src/pages/Points/index.tsx
+++ src/pages/Points/index.tsx
@@ -19,12 +19,24 @@ interface Item {
 const Points = () => {
 
     const [items, setItems] = useState<Item[]>([]);
+    const [selectedItems, setSelectedItems] = useState<number[]>([]);
 
     useEffect(() => {
         api.get('items').then(response => {
             setItems(response.data);
         });
     }, []);
+
+    function handleSelectedItem(id: number){
+        const alreadySelected = selectedItems.findIndex(item => item === id);
+
+        if (alreadySelected >= 0) {
+            const filteredItems = selectedItems.filter(item => item !== id);
+            setSelectedItems(filteredItems);
+        } else {
+            setSelectedItems([ ...selectedItems, id ]);    
+        }
+    }
 
     const navigation = useNavigation();
 
@@ -78,8 +90,11 @@ const Points = () => {
                     {items.map( item => (
                         <TouchableOpacity 
                         key={String(item.id)} 
-                        style={styles.item} 
-                        onPress={() => { }}
+                        style={[
+                            styles.item,
+                            selectedItems.includes(item.id) ? styles.selectedItem : {}   
+                        ]} 
+                        onPress={() => handleSelectedItem(item.id)}
                         activeOpacity={0.6}
                         >
                             <SvgUri width={42} height={42} uri={item.image_url} />
